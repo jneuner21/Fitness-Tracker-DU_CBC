ca1f736a50a6378057e524259b2cc6f3f9a7933a
jneuner21 <72330999+jneuner21@users.noreply.github.com>
2021-06-13 12:27:25 -0600

    built workout models, set up api routes for pages, built boilerplate for server.js

---
+++ routes/apiRoutes.js
@@ -0,0 +1,53 @@
+const router = require("express").Router();
+const db = require("../models");
+
+router.post("/api/workouts", (req, res) => {
+  db.Workout.create(req.body)
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+router.put("/api/workouts/:id", (req, res) => {
+  const id = req.params.id;
+  const body = req.body;
+  db.Workout.updateOne(
+    { _id: id },
+    {
+      $push: {
+        exercises: { ...body },
+      },
+    }
+  )
+    .then((dbWorkout) => {
+      res.status(200).json(dbWorkout);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+});
+
+router.get("/api/workouts", (req, res) => {
+  db.Workout.find()
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
+router.get("/api/workouts/range", (req, res) => {
+  db.Workout.find({})
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
+module.exports = router;
